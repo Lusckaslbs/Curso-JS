26f2071fd9866052cbb438c6d644690a6c46ae54
Luckaslbs <p.lucasbatistasilvas@gmail.com>
2023-01-11 17:34:14 -0300

    commit

---
+++ aula16/index.js
@@ -0,0 +1,19 @@
+//               0       1        2
+const alunos = ['Lucas', 'Maria', 'João'];
+
+alunos.unshift('Luiza')
+console.log(alunos);
+
+// alunos.push('Luiza');
+// alunos.push('Fábio');
+
+// alunos[alunos.length] = 'Luiza';
+// alunos[alunos.length] = 'Fabio';
+// alunos[alunos.length] = 'Luana';
+
+// alunos[0] = 'Eduardo';
+// alunos[3] = 'Luiza';
+
+// console.log(alunos);
+//console.log(alunos[0]);
+// console.log(alunos[2]);
