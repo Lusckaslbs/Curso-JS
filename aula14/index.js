d3837dcef34716c5f53fde052bd14056a40f440b
Luckaslbs <p.lucasbatistasilvas@gmail.com>
2023-01-11 14:45:25 -0300

    commit

--- aula14/index.js
+++ aula14/index.js
@@ -1,10 +1,13 @@
-let num1 = 10.25;
-let num2 = 2.5;
+// IEEE 754-2008
+let num1 = 0.7; // number
+let num2 = 0.1; // number
+
+console.log(num1 + num2);
 
 // console.log(num1.toString() + num2);
 // console.log(typeof num1);
 // console.log(num1.toString(2));
 // console.log(num1.toFixed(2));
 // console.log(Number.isInteger(num1));
-let temp = num1 * 'Ola';
-console.log(Number.isNaN(temp));
+// let temp = num1 * 'Ola';
+// console.log(temp);
